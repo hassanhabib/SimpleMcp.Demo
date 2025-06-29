ebf58e9adcc471ed494b2868307a04128e92a999
Hassan Habib <hassanhabib@live.com>
2025-06-29 15:10:21 -0700

    Add project files.

---
+++ SimpleMcp.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using MCPSharp;
+using MCPSharp.Model;
+
+namespace SimpleMcp.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new MCPClient(
+                name: "McpClient",
+                version: "v1.0.0",
+                server: "D:\\source\\repos\\SimpleMcp\\SimpleMcp.Server\\bin\\Debug\\net9.0\\SimpleMcp.Server.exe");
+
+            var result = await client.CallToolAsync(
+                name: "addition",
+                parameters: new Dictionary<string, object>
+                {
+                    {"firstNumber", 5 },
+                    {"secondNumber", 10 }
+                });
+
+            Console.WriteLine(result.Content[0].Text);
+        }
+    }
+}
---
+++ SimpleMcp.Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using MCPSharp;
+
+namespace SimpleMcp.Server
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            MCPServer.Register<CalculatorTool>();
+
+            await MCPServer.StartAsync(
+                serverName: "SimpleMcp.Server",
+                version: "v1.0.0");
+        }
+    }
+
+    public class CalculatorTool
+    {
+        [McpTool(name: "addition", Description = "This tool will add two numbers.")]
+        public static int Addition(
+            [McpParameter(required: true, description: "First number")] int firstNumber,
+            [McpParameter(required: true, description: "Second number")] int secondNumber)
+        {
+            return firstNumber + secondNumber + 100;
+        }
+    }
+}
